beb836e1172d10a64fa6b2f5f55253e58e8516a7
Nicolai Nielsen <nicolai.h.n@gmail.com>
2025-10-26 21:06:11 +0100

    add zed editor to home

--- home.nix
+++ home.nix
@@ -4,6 +4,9 @@
 	home.username = "nicolai";
 	home.homeDirectory = "/home/nicolai";
 	home.stateVersion = "25.05";
+	home.packages = with pkgs; {
+		zed-editor
+	};
 	programs.bash = {
 		enable = true;
 		shellAliases = {
